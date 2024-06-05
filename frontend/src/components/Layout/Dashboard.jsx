8e9580cd47eb9cf0ad1e661dc33c9fe9ae5b89de
Rajan Dubey <rdrajan9452@gmail.com>
2024-06-05 22:22:36 +0530

    Create Dashboard.jsx
    made a user dashboard

---
+++ frontend/src/components/Layout/Dashboard.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { Context } from "../../main";
+import "./Dashboard.css"; // Import the CSS file
+
+const Dashboard = () => {
+  const { user } = useContext(Context);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:4000/api/v1/user/getuser",
+          {
+            withCredentials: true,
+          }
+        );
+        setUserInfo(res.data.user);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <div className="profile">
+        <h2>{userInfo.name}</h2>
+        <p>Email: {userInfo.email}</p>
+        <p>Phone: {userInfo.phone}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
