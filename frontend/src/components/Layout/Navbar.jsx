a9aee5104e5021957ec8532ae47766c9bbcc121a
Rajan Dubey <rdrajan9452@gmail.com>
2024-06-05 22:22:40 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,17 +1,200 @@
-import React, { useContext, useEffect, useState } from "react";
+// import React, { useContext, useEffect, useState } from "react";
+// import { Context } from "../../main";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import toast from "react-hot-toast";
+// import { GiHamburgerMenu } from "react-icons/gi";
+// //import './navbar.css'; // Ensure this import points to your CSS file
+
+// const Navbar = () => {
+//   const [show, setShow] = useState(false);
+//   const { isAuthorized, setIsAuthorized, user } = useContext(Context);
+//   const navigateTo = useNavigate();
+
+//   const handleLogout = async () => {
+//     try {
+//       const response = await axios.get(
+//         "http://localhost:4000/api/v1/user/logout",
+//         {
+//           withCredentials: true,
+//         }
+//       );
+//       toast.success(response.data.message);
+//       setIsAuthorized(false);
+//       navigateTo("/login");
+//     } catch (error) {
+//       toast.error(error.response.data.message), setIsAuthorized(true);
+//     }
+//   };
+
+//   return (
+//     <nav className={isAuthorized ? "navbarShow" : "navbarHide"}>
+//       <div className="container">
+//         <div className="logo">
+//           <img src="/JobZee-logos__white__-removebg-preview.png" alt="logo" style={{  height: '8rem' }} />
+
+//         </div>
+//         <ul className={!show ? "menu" : "show-menu menu"}>
+//           <li>
+//             <Link to={"/"} onClick={() => setShow(false)}>
+//               HOME
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to={"/job/getall"} onClick={() => setShow(false)}>
+//               ALL JOBS
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to={"/applications/me"} onClick={() => setShow(false)}>
+//               {user && user.role === "Manager"
+//                 ? "APPLICANT'S APPLICATIONS"
+//                 : "MY APPLICATIONS"}
+//             </Link>
+//           </li>
+//           {user && user.role === "Manager" ? (
+//             <>
+//               <li>
+//                 <Link to={"/job/post"} onClick={() => setShow(false)}>
+//                   POST NEW JOB
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link to={"/job/me"} onClick={() => setShow(false)}>
+//                   VIEW YOUR JOBS
+//                 </Link>
+//               </li>
+//             </>
+//           ) : (
+//             <></>
+//           )}
+
+//           <button onClick={handleLogout}>LOGOUT</button>
+//         </ul>
+//         <div className="hamburger">
+//           <GiHamburgerMenu onClick={() => setShow(!show)} />
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+// import React, { useContext, useState } from "react";
+// import { Context } from "../../main";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import toast from "react-hot-toast";
+// import { GiHamburgerMenu } from "react-icons/gi";
+// import Loading from "../Layout/Loading";
+
+// const Navbar = () => {
+//   const [show, setShow] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const { isAuthorized, setIsAuthorized, user } = useContext(Context);
+//   const navigateTo = useNavigate();
+
+//   const handleLogout = async () => {
+//     setLoading(true);
+//     try {
+//       const response = await axios.get(
+//         "http://localhost:4000/api/v1/user/logout",
+//         {
+//           withCredentials: true,
+//         }
+//       );
+//       toast.success(response.data.message);
+//       setIsAuthorized(false);
+//       navigateTo("/login");
+//     } catch (error) {
+//       toast.error(error.response.data.message);
+//       setIsAuthorized(true);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <Loading show={loading} />
+//       <nav className={isAuthorized ? "navbarShow" : "navbarHide"}>
+//         <div className="container">
+//           <div className="logo">
+//             <img
+//               src="/JobZee-logos__white__-removebg-preview.png"
+//               alt="logo"
+//               style={{ height: "8rem" }}
+//             />
+//           </div>
+//           <ul className={!show ? "menu" : "show-menu menu"}>
+//             <li>
+//               <Link to={"/"} onClick={() => setShow(false)}>
+//                 HOME
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to={"/job/getall"} onClick={() => setShow(false)}>
+//                 ALL JOBS
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to={"/applications/me"} onClick={() => setShow(false)}>
+//                 {user && user.role === "Manager"
+//                   ? "APPLICANT'S APPLICATIONS"
+//                   : "MY APPLICATIONS"}
+//               </Link>
+//             </li>
+//             {user && user.role === "Manager" ? (
+//               <>
+//                 <li>
+//                   <Link to={"/job/post"} onClick={() => setShow(false)}>
+//                     POST NEW JOB
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link to={"/job/me"} onClick={() => setShow(false)}>
+//                     VIEW YOUR JOBS
+//                   </Link>
+//                 </li>
+//               </>
+//             ) : (
+//               <></>
+//             )}
+//             <button onClick={handleLogout}>LOGOUT</button>
+//           </ul>
+//           <div className="hamburger">
+//             <GiHamburgerMenu onClick={() => setShow(!show)} />
+//           </div>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+
+import React, { useContext, useState } from "react";
 import { Context } from "../../main";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { GiHamburgerMenu } from "react-icons/gi";
-//import './navbar.css'; // Ensure this import points to your CSS file
+import Loading from "../Layout/Loading";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { isAuthorized, setIsAuthorized, user } = useContext(Context);
   const navigateTo = useNavigate();
 
   const handleLogout = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(
         "http://localhost:4000/api/v1/user/logout",
@@ -23,63 +206,78 @@ const Navbar = () => {
       setIsAuthorized(false);
       navigateTo("/login");
     } catch (error) {
-      toast.error(error.response.data.message), setIsAuthorized(true);
+      toast.error(error.response.data.message);
+      setIsAuthorized(true);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <nav className={isAuthorized ? "navbarShow" : "navbarHide"}>
-      <div className="container">
-        <div className="logo">
-          <img src="/JobZee-logos__white__-removebg-preview.png" alt="logo" style={{  height: '8rem' }} />
-
+    <>
+      <Loading show={loading} />
+      <nav className={isAuthorized ? "navbarShow" : "navbarHide"}>
+        <div className="container">
+          <div className="logo">
+            <img
+              src="/JobZee-logos__white__-removebg-preview.png"
+              alt="logo"
+              style={{ height: "8rem" }}
+            />
+          </div>
+          <ul className={!show ? "menu" : "show-menu menu"}>
+            <li>
+              <Link to={"/"} onClick={() => setShow(false)}>
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link to={"/job/getall"} onClick={() => setShow(false)}>
+                ALL JOBS
+              </Link>
+            </li>
+            <li>
+              <Link to={"/applications/me"} onClick={() => setShow(false)}>
+                {user && user.role === "Manager"
+                  ? "APPLICANT'S APPLICATIONS"
+                  : "MY APPLICATIONS"}
+              </Link>
+            </li>
+            {user && user.role === "Manager" ? (
+              <>
+                <li>
+                  <Link to={"/job/post"} onClick={() => setShow(false)}>
+                    POST NEW JOB
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/job/me"} onClick={() => setShow(false)}>
+                    VIEW YOUR JOBS
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <></>
+            )}
+            <li>
+              <Link to={"/dashboard"} onClick={() => setShow(false)}>
+                <img
+                  src="./user.png"
+                  alt="User"
+                  style={{ height: "40px", borderRadius: "50%" }}
+                />
+                {user?.name}
+              </Link>
+            </li>
+            <button onClick={handleLogout}>LOGOUT</button>
+          </ul>
+          <div className="hamburger">
+            <GiHamburgerMenu onClick={() => setShow(!show)} />
+          </div>
         </div>
-        <ul className={!show ? "menu" : "show-menu menu"}>
-          <li>
-            <Link to={"/"} onClick={() => setShow(false)}>
-              HOME
-            </Link>
-          </li>
-          <li>
-            <Link to={"/job/getall"} onClick={() => setShow(false)}>
-              ALL JOBS
-            </Link>
-          </li>
-          <li>
-            <Link to={"/applications/me"} onClick={() => setShow(false)}>
-              {user && user.role === "Manager"
-                ? "APPLICANT'S APPLICATIONS"
-                : "MY APPLICATIONS"}
-            </Link>
-          </li>
-          {user && user.role === "Manager" ? (
-            <>
-              <li>
-                <Link to={"/job/post"} onClick={() => setShow(false)}>
-                  POST NEW JOB
-                </Link>
-              </li>
-              <li>
-                <Link to={"/job/me"} onClick={() => setShow(false)}>
-                  VIEW YOUR JOBS
-                </Link>
-              </li>
-            </>
-          ) : (
-            <></>
-          )}
-
-          <button onClick={handleLogout}>LOGOUT</button>
-        </ul>
-        <div className="hamburger">
-          <GiHamburgerMenu onClick={() => setShow(!show)} />
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
 export default Navbar;
-
-
-
