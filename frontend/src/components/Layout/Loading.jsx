786cb3592307b32b0f8f0af821e1d986f29df4df
Rajan Dubey <rdrajan9452@gmail.com>
2024-06-05 18:45:54 +0530

    Added loader
    Created loading component that wiil be shown when api fetches data

--- frontend/src/components/Layout/Loading.jsx
+++ frontend/src/components/Layout/Loading.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import "./Loading.css"; // Ensure you have appropriate CSS for the loader
 
-const Loading = () => {
-  return (
+const Loading = ({show}) => {
+  return show && (
     <div className="loading-overlay">
       <div className="loading-spinner"></div>
     </div>
